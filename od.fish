1f6f7e80d1087c896b4366ca278d549f40497301
clintcparker <clintcparker@gmail.com>
2018-08-29 17:57:25 -0700

    updated od.fish

--- od.fish
+++ od.fish
@@ -1,5 +1,6 @@
 function od --description 'Deploy an octopress site in production configuration'
 	set -gx JEKYLL_ENV 'production'
+    jekyll build
     octopress deploy
     set -gx JEKYLL_ENV 'development'
 end
