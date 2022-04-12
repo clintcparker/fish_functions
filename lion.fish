4cdf24080105823a885e59362d42dab06c5b32e4
Clint Parker <clintcparker@gmail.com>
2022-04-12 13:59:30 -0700

    fixed litraglow commands to use local hidapitester

--- lion.fish
+++ lion.fish
@@ -1,7 +1,7 @@
 function lion
-    ~/.hidapi/hidapitester/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
-    ~/.hidapi/hidapitester/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
-    ~/.hidapi/hidapitester/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
-    ~/.hidapi/hidapitester/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
-    ~/.hidapi/hidapitester/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
+    ~/.bin/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
+    ~/.bin/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
+    ~/.bin/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
+    ~/.bin/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
+    ~/.bin/hidapitester --vidpid 046D/C900 --open --length 20 --send-output 0x11,0xff,0x04,0x1c,0x01
 end
