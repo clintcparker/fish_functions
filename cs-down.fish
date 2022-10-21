399ed03eac02cf03c42f178d56c5cab0e124f41a
Clint Parker <clintcparker@gmail.com>
2022-10-21 15:17:04 -0700

    more ssh

--- cs-down.fish
+++ cs-down.fish
@@ -1,3 +1,3 @@
-function ss1-down --wraps='ssh -TO exit ss1' --description 'alias ss1-down=ssh -TO exit ss1'
-  ssh -TO exit ss1 $argv; 
+function cs-down --wraps='ssh -TO exit cs' --description 'alias cs-down=ssh -TO exit cs'
+  ssh -TO exit cs $argv; 
 end
