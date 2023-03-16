7b4d74eef0750e7b8e64ce7766d35d4bb53957fb
Clint Parker <clintcparker@gmail.com>
2023-03-16 15:40:16 -0700

    added dotnet variable updater

---
+++ asdf_update_dotnet_home.fish
@@ -0,0 +1,8 @@
+function asdf_update_dotnet_home --on-event fish_prompt
+  set --local dotnet_path (asdf which dotnet)
+  if test -n "$dotnet_path"
+    set -gx DOTNET_ROOT (asdf where dotnet)
+    set --local dotnet_version (dotnet --version)
+    set -gx MSBuildSDKsPath (realpath "$DOTNET_ROOT/sdk/$dotnet_version/Sdks")
+  end
+end
